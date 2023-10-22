e987826d27d6114bed130e123e1a52fa7f6e02ba
robert <robert.mueller-uri@gmx.de>
2023-10-22 10:22:26 +0200

    added more streams tasks to ExtendedSexyStreams

---
+++ src/exercises/exercise01_mini_stream_functions/ExtendedSexyStreams.java
@@ -0,0 +1,42 @@
+package exercises.exercise01_mini_stream_functions;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ExtendedSexyStreams implements extendedStreamOrLoop{
+
+    @Override
+    public long productOfList(List<Integer> myNumbers) {
+        return 0;
+    }
+
+    @Override
+    public Optional<Integer> secondHighestNumber(List<Integer> myNumbers) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Integer> filterOdds(List<Integer> myNumbers) {
+        return null;
+    }
+
+    @Override
+    public boolean isPalindrome(int number) {
+        return false;
+    }
+
+    @Override
+    public Optional<Integer> mostFrequentNumber(List<Integer> myNumbers) {
+        return Optional.empty();
+    }
+
+    @Override
+    public long sumOfSquares(List<Integer> myNumbers) {
+        return 0;
+    }
+
+    @Override
+    public List<Integer> doubleValues(List<Integer> myNumbers) {
+        return null;
+    }
+}
