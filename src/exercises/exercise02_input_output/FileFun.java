2702642aaba775e5d2bad9103dcb121defe40132
hm-ischmid <ischmid@hm.edu>
2023-10-29 10:07:13 +0100

    add file and path tasks

---
+++ src/exercises/exercise02_input_output/FileFun.java
@@ -0,0 +1,27 @@
+package exercises.exercise02_input_output;
+
+
+import java.nio.file.Path;
+
+public class FileFun implements FileAndPath{
+
+    @Override
+    public void hemmingDistance(Path file1, Path file2) {
+
+    }
+
+    @Override
+    public void convertToLowercase(Path inPath, Path outPath) {
+
+    }
+
+    @Override
+    public void readLineByLine(Path file) {
+
+    }
+
+    @Override
+    public void countWordsOfFile(Path file) {
+
+    }
+}
