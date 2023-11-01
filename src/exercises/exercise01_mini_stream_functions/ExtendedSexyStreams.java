2959cf407e4231186cf8320b03d7287f10853e31
leonbender <bender1@hm.edu>
2023-11-01 11:06:21 +0100

    save changes leon

--- src/exercises/exercise01_mini_stream_functions/ExtendedSexyStreams.java
+++ src/exercises/exercise01_mini_stream_functions/ExtendedSexyStreams.java
@@ -2,22 +2,26 @@ package exercises.exercise01_mini_stream_functions;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ExtendedSexyStreams implements extendedStreamOrLoop{
 
     @Override
     public long productOfList(List<Integer> myNumbers) {
-        return 0;
+        return myNumbers.stream().reduce((a,b) -> a*b).get();
     }
 
     @Override
     public Optional<Integer> secondHighestNumber(List<Integer> myNumbers) {
-        return Optional.empty();
+
+        List<Integer> myInteger = myNumbers.stream().sorted(Integer::compareTo).skip(Math.abs(myNumbers.size()-2)).toList();
+        return myInteger.size() > 1 ? Optional.of(myInteger.get(myInteger.size() -2)) : Optional.empty();
     }
 
     @Override
     public List<Integer> filterOdds(List<Integer> myNumbers) {
-        return null;
+        return myNumbers.stream().filter(num -> num%2 != 0).toList();
     }
 
     @Override
@@ -27,11 +31,13 @@ public class ExtendedSexyStreams implements extendedStreamOrLoop{
 
     @Override
     public Optional<Integer> mostFrequentNumber(List<Integer> myNumbers) {
+        myNumbers.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
         return Optional.empty();
     }
 
     @Override
     public long sumOfSquares(List<Integer> myNumbers) {
+
         return 0;
     }
 
